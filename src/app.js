67b4e4d5203af92e5386b3c8feb56f3a843b8614
WonganiB <mkwbanda@yahoo.co.uk>
2023-06-30 20:25:41 +0100

    added  html, css and js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
-alert("hello Mirriam");
+function ThingsToDo() {
+  let group = prompt("How many in your group");
+  if (group > 4) {
+    alert("10% Off");
+  } else {
+    alert("No discount Available");
+  }
+  let email = prompt("What is your email address");
+
+  alert("Thank you!" + "will contact you shortly");
+}
+let ThingsToDobutton = document.querySelector("button");
+ThingsToDobutton.addEventListener("click", ThingsToDo);
